8b6c79a153c780536e819bb53ddc496f1bb7802d
HeeMang28 <22tkakd@naver.com>
2022-04-14 22:07:46 +0900

    Servlet 2일차

--- JSPBasic/src/main/webapp/servletForm/springForm.jsp
+++ JSPBasic/src/main/webapp/servletForm/springForm.jsp
@@ -9,9 +9,10 @@
 <body>
 	<!--  http://localhost:8181/JSPBasic/spring을 목적지로 하는 폼을 만들어주세요. 
 	좀전처럼 jsp, boot 라는 이름으로 submit을 하는 폼을 생성해주세요. -->
-	<form action="http://localhost:8181/JSPBasic/spring" method="post">
+	<form action="http://localhost:8181/JSPBasic/spring" />
 		jsp 파라미터 값 : <input type="text" name="jsp" /><br/>
 		boot 파라미터 값 : <input type="text" name="boot" /> <br/>
+		jpa 파라미터 값 : <input type="text" name="jpa" /> <br/>
 		<input type="submit" value="확인"> <br/>
 	</form>
 </body>
--- MyFirstWeb/src/main/java/kr/co/ict/ServletCustom.java
+++ MyFirstWeb/src/main/java/kr/co/ict/ServletCustom.java
@@ -42,11 +42,14 @@ public class ServletCustom extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+	// get방식으로 들어오는 접근만 처리
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("spring 접속 감지");
 		// request.getParamter()를 이용해
+		// "jsp", "boot" 라는 이름으로 들어오는 요소를 콘솔에 찍도록 아래에 콘솔을 작성하고
 		String jsp = request.getParameter("jsp");
 		String boot = request.getParameter("boot");
+		String jpa = request.getParameter("jpa");
 		System.out.println("jsp파라미터 값 : " + jsp);
 		System.out.println("boot파라미터 값 : " + boot);
 		// 들어온 데이터를 콘솔이 아닌 결과화면에 찍어보겠습니다.
@@ -58,6 +61,7 @@ public class ServletCustom extends HttpServlet {
 		// 포워딩 절차 1. 보내고 싶은 데이터를 request.setAttribute("저장명", 자료); 형식으로 저장
 		request.setAttribute("jsp", jsp);
 		request.setAttribute("boot", boot);
+		request.setAttribute("jpa", jpa);
 		// 카카오톡에서 전송버튼 안누른상태
 		// request.setAttribute로 싫어놓은 변수를 결과페이지로 보내기 위해 forward를 
 		// 목적지 주소는 http://localhost:8181/JSPBasic 을 생략한 나머지 주소만 적습니다.
@@ -71,9 +75,23 @@ public class ServletCustom extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	// post방식으로 들어오는 접근만 처리
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		System.out.println("포스트방식 접근 감지");
+		// 한글이 안 깨지도록 처리
+		request.setCharacterEncoding("utf-8");
+		// 폼에서 보낸 데이터 자바 변수로 저장
+		String jsp = request.getParameter("jsp");
+		String boot = request.getParameter("boot");
+		String jpa = request.getParameter("jpa"); 
+		// request.setAttribute로 받은 변수들 보낼 준비
+		request.setAttribute("jsp", jsp);
+		request.setAttribute("boot", boot);
+		request.setAttribute("jpa", jpa);
+		// 포워딩 주소 세팅 springResult.jsp를 목표지점으로
+		RequestDispatcher dp = request.getRequestDispatcher("/servletForm/springResult.jsp");
+		// 포워딩
+		dp.forward(request, response);
 	}
 
 }
